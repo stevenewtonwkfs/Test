00fd1190b726add10f5683369e81d0e817e9b813
stevenewtonwkfs <steve.harris@wolterskluwer.com>
2017-06-01 15:56:44 +0100

    initial

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello Steve");
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
